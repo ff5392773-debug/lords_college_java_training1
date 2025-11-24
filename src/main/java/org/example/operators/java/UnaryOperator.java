fe813c5d573988a2e20fc73a6f8df1e1ec8f7f02
fouzia-coder <fouziafatima929@gmail.com>
2025-11-24 15:56:41 +0530

    Added all operator Java files

---
+++ src/main/java/org/example/operators/java/UnaryOperator.java
@@ -0,0 +1,16 @@
+package org.example.operators.java;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int a = -10;
+        System.out.println(a);
+        int b = +10;
+        System.out.println(b);
+        ++b;
+        System.out.println(b);
+        --b;
+        System.out.println(b);
+
+    }
+}
+
